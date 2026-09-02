b6e6052c9204af67fbe4c23b6f437608a6e3f177
mafiullahhasan <jayenhasan2003@gmail.com>
2026-09-02 11:37:22 +0600

    add all tables query

--- main.sql
+++ main.sql
@@ -25,3 +25,34 @@ CREATE TABLE matches (
         )
         
 );
+
+-- create bookings table 
+CREATE TABLE bookings (
+    booking_id SERIAL primary key,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(20) NOT NULL,
+    payment_status VARCHAR(20) NOT NULL,
+    total_cost DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT bookings_user_fk
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id),
+
+    CONSTRAINT bookings_match_fk
+        FOREIGN KEY (match_id)
+        REFERENCES matches(match_id),
+
+    CONSTRAINT bookings_cost_check
+        CHECK (total_cost >= 0),
+
+    CONSTRAINT bookings_payment_status_check
+        CHECK (
+            payment_status IN (
+                'Pending',
+                'Confirmed',
+                'Cancelled',
+                'Refunded'
+            )
+        )
+);
